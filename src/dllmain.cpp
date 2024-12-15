f43353399b1a3d72796cd9a83dfa6d47bc024db6
vsaint1 <vsantos1.dev@gmail.com>
2024-12-15 05:17:00 -0300

    fix pattern scanner and post_render

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,49 +1,102 @@
 #include "Utils.h"
 #include "gui/Menu.h"
 #include "mem/Memory.h"
+#include <Psapi.h>
 
 using PostRenderFn = void (*)(SDK::UGameViewportClient *, SDK::UCanvas *);
 
 PostRenderFn originalPostRenderFn = 0;
 
-void PostRenderHooked(SDK::UGameViewportClient *_this, SDK::UCanvas *pCanvas) {
+void PostRenderHooked(SDK::UGameViewportClient *_this, SDK::UCanvas *canvas) {
 
-  pCanvas->K2_DrawLine({100.f, 100.f}, {200.f, 200.0f}, 1.0f,
-                       {1.0f, 1.0f, 1.0f, 1.0f});
+  static SDK::FVector2D center{300.0f, 300.0f};
+  static float size = 50.0f;
+  static float thickness = 1.0f;
 
-  pCanvas->K2_DrawText(pFont, L"[ANGOLA_HOOK]", {100.f, 20.f}, {1.f, 1.f},
+  // static SDK::FLinearColor color = {1.0f, 0.0f, 0.0f, 0.0f};
+
+  // float halfSize = size / 2.0f;
+  // float armOffset = size / 4.0f;
+
+  //// Horizontal and vertical lines
+  // canvas->K2_DrawLine({center.X - halfSize, center.Y},
+  //                      {center.X + halfSize, center.Y}, thickness,
+  //                      color); // Horizontal
+  // canvas->K2_DrawLine({center.X, center.Y - halfSize},
+  //                      {center.X, center.Y + halfSize}, thickness,
+  //                      color); // Vertical
+
+  //// Top-left arm
+  // canvas->K2_DrawLine({center.X - halfSize, center.Y - halfSize},
+  //                      {center.X - halfSize + armOffset, center.Y -
+  //                      halfSize}, thickness, color); // Horizontal
+  // canvas->K2_DrawLine({center.X - halfSize, center.Y - halfSize},
+  //                      {center.X - halfSize, center.Y - halfSize +
+  //                      armOffset}, thickness, color); // Vertical
+
+  //// Top-right arm
+  // canvas->K2_DrawLine({center.X + halfSize, center.Y - halfSize},
+  //                      {center.X + halfSize - armOffset, center.Y -
+  //                      halfSize}, thickness, color); // Horizontal
+  // canvas->K2_DrawLine({center.X + halfSize, center.Y - halfSize},
+  //                      {center.X + halfSize, center.Y - halfSize +
+  //                      armOffset}, thickness, color); // Vertical
+
+  //// Bottom-left arm
+  // canvas->K2_DrawLine({center.X - halfSize, center.Y + halfSize},
+  //                      {center.X - halfSize + armOffset, center.Y +
+  //                      halfSize}, thickness, color); // Horizontal
+  // canvas->K2_DrawLine({center.X - halfSize, center.Y + halfSize},
+  //                      {center.X - halfSize, center.Y + halfSize -
+  //                      armOffset}, thickness, color); // Vertical
+
+  //// Bottom-right arm
+  // canvas->K2_DrawLine({center.X + halfSize, center.Y + halfSize},
+  //                      {center.X + halfSize - armOffset, center.Y +
+  //                      halfSize}, thickness, color); // Horizontal
+  // canvas->K2_DrawLine({center.X + halfSize, center.Y + halfSize},
+  //                      {center.X + halfSize, center.Y + halfSize -
+  //                      armOffset}, thickness, color); // Vertical
+
+  canvas->K2_DrawText(pFont, L"[ANGOLA_HOOK]", {100.f, 20.f}, {1.f, 1.f},
                        {1.0f, 1.0f, 1.0f, 1.0f}, 0.0f, {1.0f, 1.0f}, {200, 200},
                        true, true, true, {0.0f, 0.0f, 0.0f, 0.5f});
 
-  auto size = Utils::GetScreenSize();
+  auto sSize = Utils::GetScreenSize();
 
   std::stringstream sScreenSize;
 
-  sScreenSize << "[" << size.width << " " << size.height << "]";
+  sScreenSize << "[" << sSize.width << " " << sSize.height << "]";
 
-  pCanvas->K2_DrawText(pFont, Utils::ToWString(sScreenSize.str()).c_str(),
+  canvas->K2_DrawText(pFont, Utils::ToWString(sScreenSize.str()).c_str(),
                        {200.f, 20.f}, {1.f, 1.f}, {0.5f, 0.3f, 1.0f, 1.0f},
                        0.0f, {1.0f, 1.0f}, {200, 200}, true, true, true,
                        {0.0f, 0.0f, 0.0f, 0.5f});
 
-  ZeroGUI::SetupCanvas(pCanvas);
+  ZeroGUI::SetupCanvas(canvas);
   Menu::Update();
 
   if (originalPostRenderFn) {
-    originalPostRenderFn(_this, pCanvas);
+    originalPostRenderFn(_this, canvas);
   }
 }
 
 static unsigned long FakEntry(HMODULE hModule) {
+
   AllocConsole();
   FILE *pFile{nullptr};
   freopen_s(&pFile, "CONOUT$", "w", __acrt_iob_func(1));
 
-  U_LOG("[ANGOLA_HOOK] injected %s", Utils::GetWndowTitle().c_str());
+  U_LOG("[ANGOLA_HOOK] injected %s\n", Utils::GetWndowTitle().c_str());
+  uintptr_t moduleBase = (uintptr_t)GetModuleHandle(0);
+
+  auto UWorld = Memory::FindPattern(
+      (PBYTE) "\x48\x89\x05\x00\x00\x00\x00\x48\x8B\xB7\x00\x00\x00\x00",
+      "xxx????xxx????", 0x3, 0x7);
 
   SDK::UEngine *pEngine = SDK::UEngine::GetEngine();
 
-  SDK::UWorld *pWorld = SDK::UWorld::GetWorld();
+  SDK::UWorld *pWorld = *(SDK::UWorld **)UWorld;
 
   if (!pWorld || !pEngine) {
     U_LOG("Failed to get world and engine");
@@ -59,6 +112,9 @@ static unsigned long FakEntry(HMODULE hModule) {
 
   originalPostRenderFn = reinterpret_cast<decltype(originalPostRenderFn)>(
       Memory::VMT_Hook(viewportVtable, 0x63, &PostRenderHooked));
+
+  U_LOG("[%p] GWorld \n", UWorld);
+  U_LOG("[%p] UGameViewportClient->DrawTransition", originalPostRenderFn);
 
   while (!GetAsyncKeyState(VK_END)) {
 
--- src/mem/Memory.h
+++ src/mem/Memory.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Utils.h"
+#include <Psapi.h>
 
 namespace Memory {
 template <typename T> T Read(uintptr_t address) { return *(T *)address; }
@@ -10,12 +11,6 @@ template <typename T> void Write(uintptr_t address, const T &value) {
 
 uintptr_t ResolveAddress(uintptr_t baseAddress,
                          const std::vector<uintptr_t> &offsets);
-
-uint8_t *FindPattern(const char *moduleName, const char *sig,
-                     unsigned int byteOffset);
-
-uintptr_t FindPatternOffset(const char *moduleName, const char *sig,
-                            unsigned int byteOffset = 0u);
 
 inline char *VMT_Hook(void **vtable, uint32_t index, void *targetFunction) {
 
@@ -31,6 +26,50 @@ inline char *VMT_Hook(void **vtable, uint32_t index, void *targetFunction) {
   VirtualProtect(&vtable[index], sizeof(void *), oldProtection, 0);
 
   return original;
+}
+
+inline bool _Mask(BYTE *address, PBYTE signature, const char *mask) {
+  for (; *mask; ++mask, ++address, ++signature) {
+    if (*mask == 'x' && *address != *signature)
+      return false;
+  }
+  return (*mask) == 0;
+}
+
+inline PBYTE FindPattern(BYTE *signature, const char *mask,
+                         std::int32_t instructionSize,
+                         std::int32_t relativeOffset,
+                         const wchar_t *moduleName = nullptr) {
+
+  HMODULE module{};
+
+  if (!moduleName)
+    module = GetModuleHandle(moduleName);
+  else
+    module = GetModuleHandle(0);
+
+  MODULEINFO mInfo{};
+  K32GetModuleInformation(GetCurrentProcess(), module, &mInfo, sizeof(mInfo));
+
+  BYTE *baseAddress = (BYTE *)mInfo.lpBaseOfDll;
+  DWORD dwSize = mInfo.SizeOfImage;
+  size_t length = strlen(mask);
+
+  for (size_t i = 0; i < dwSize - length; i++) {
+    if (_Mask(baseAddress + i, signature, mask)) {
+      BYTE *matchAddress = baseAddress + i;
+
+      std::int32_t relativeAddress =
+          *(std::int32_t *)(matchAddress + instructionSize);
+
+      U_LOG("[%p] RELATIVE_OFFSET\n", relativeOffset);
+
+      BYTE *address = matchAddress + relativeAddress +relativeOffset;
+
+      return address;
+    }
+  }
+  return nullptr;
 }
 
 } // namespace Memory
